ff182a8595258164ac18b48be53e4c293c2c749e
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-19 19:31:31 +0530

    updating ./tf-module-rabbitmq

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "rabbitmq" {
   ami   = data.aws_ami.centos-8-ami.image_id
   instance_type = var.instance_type
-  subnet_id = var.subnets
+  subnet_id = var.subnets[0]
   tags = {
     Name = "${var.env}-${var.name} -rabbitmq"
   }
