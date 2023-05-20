27a49c0b850b1c5b41acd68414f8d72d666acaf6
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-20 16:02:48 +0530

    updating ./tf-module-rabbitmq

--- main.tf
+++ main.tf
@@ -7,18 +7,35 @@ resource "aws_instance" "rabbitmq" {
     Name = "${var.env}-${var.name} -rabbitmq"
   }
 
-  provisioner "remote-exec" {
-    connection {
-      host = self.private_ip
-      user = local.SSH_USER
-      password = local.SSH_PASS
+#  provisioner "remote-exec" {
+#    connection {
+#      host = self.private_ip
+#      user = local.SSH_USER
+#      password = local.SSH_PASS
+#    }
+#
+#    inline = {
+#      "sudo yum install python39-devel -y" ,
+#      " sudo pip3.9 install ansible botocore boto3" ,
+#     " ansible-pull -i localhost, -U https://github.com/sumayya-005/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${var.env}"
+#
+#  }
+#  }
+}
+
+resource "null_resource" "ansible-apply" {
+    provisioner "remote-exec" {
+      connection {
+        host = aws_instance.rabbitmq.private_ip
+        user = local.SSH_USER
+        password = local.SSH_PASS
+      }
+
+      inline = {
+        "sudo yum install python39-devel -y" ,
+        " sudo pip3.9 install ansible botocore boto3" ,
+       " ansible-pull -i localhost, -U https://github.com/sumayya-005/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${var.env}"
+
     }
-
-    inline = {
-      "sudo yum install python39-devel -y" ,
-      " sudo pip3.9 install ansible botocore boto3" ,
-     " ansible-pull -i localhost, -U https://github.com/sumayya-005/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${var.env}"
-
-  }
-  }
+    }
 }
