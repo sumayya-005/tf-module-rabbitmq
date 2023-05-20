b2e3d4a75861993a63de763dde9caea6a4115e7e
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-20 15:39:23 +0530

    updating ./tf-module-rabbitmq

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@ resource "aws_instance" "rabbitmq" {
 
   provisioner "remote-exec" {
     connection {
-      host
+      host = self.private_ip
+      user = local.SSH_USER
+      pass = local.SSH_PASS
     }
   }
 }
