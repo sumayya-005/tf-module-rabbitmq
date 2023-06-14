c487fd75bc80348e7a5c9ec1cb32bc3592537564
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-06-14 16:00:06 +0530

    updating ./tf-module-rabbitmq

--- main.tf
+++ main.tf
@@ -24,19 +24,19 @@ resource "aws_instance" "rabbitmq" {
 #  }
 }
 
-#resource "null_resource" "ansible-apply" {
-#    provisioner "remote-exec" {
-#      connection {
-#        host = aws_instance.rabbitmq.private_ip
-#        user = local.SSH_USER
-#        password = local.SSH_PASS
-#      }
-#
-#      inline = {
-#        "sudo yum install python39-devel -y" ,
-#        " sudo pip3.9 install ansible botocore boto3" ,
-#       " ansible-pull -i localhost, -U https://github.com/sumayya-005/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${var.env}"
-#
-#    }
-#    }
-#}
+resource "null_resource" "ansible-apply" {
+    provisioner "remote-exec" {
+      connection {
+        host = aws_instance.rabbitmq.private_ip
+        user = local.SSH_USER
+        password = local.SSH_PASS
+      }
+
+      inline = {
+        "sudo yum install python39-devel -y" ,
+        " sudo pip3.9 install ansible botocore boto3" ,
+       " ansible-pull -i localhost, -U https://github.com/sumayya-005/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${var.env}"
+
+    }
+    }
+}
